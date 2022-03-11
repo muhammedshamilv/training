446473a88af2f7102e671d7e51332c151d0ad309
shamil <98076229+muhammedshamilv@users.noreply.github.com>
2022-03-11 16:09:41 +0530

    Update test_programs.py

--- ari/test_programs.py
+++ ari/test_programs.py
@@ -1,23 +1,13 @@
 import programs
 
-def test_emptyString():
-     ret=programs.paragraph('')
-     assert ret==0
-def test_alphanumeric():
-     ret=programs.alphanumeric('!@#$%^')
-     assert ret ==False
 def test_numbercharectors():
      ret=programs.numbercharectors('shamil123')
      assert ret==9
-def test_emptywords():
-     ret=programs.emptywords('word')
-     assert ret==None
+
 def test_numberword():
      ret=programs.numberword('how are you')
      assert ret==2
-def test_emptysentence():
-     ret=programs.emptysentence('hope you are doing well')
-     assert ret==None
+
 def test_sentence():
      ret=programs.sentences('i how are you? hope you are doing well. please be careful!')
      assert ret==3
